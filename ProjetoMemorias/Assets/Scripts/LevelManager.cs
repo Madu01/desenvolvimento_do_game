a818f52d708e0a2781ebcac9abf3df213b801f43
Gabriel Mariano <mariano.gabriel2q@gmail.com>
2021-03-11 21:24:24 -0300

    Tentativa de Upload do Pacote do Jogo
    Tentativa de Upload do Pacote do Jogo

---
+++ ProjetoMemorias/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour{
+    public void LoadScene(string nome){
+        SceneManager.LoadScene(nome);
+    }
+
+    public void EncerrarJogo(){
+        Application.Quit();
+    }
+}
