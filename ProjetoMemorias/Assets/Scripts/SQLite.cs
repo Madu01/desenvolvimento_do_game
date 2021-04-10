1cd92db15e23c2c4cad036d14bb7639b52e59a20
Gabriel Mariano <mariano.gabriel2q@gmail.com>
2021-04-10 19:37:33 -0300

    Atualização no Banco de Dados - Funcional

--- ProjetoMemorias/Assets/Scripts/SQLite.cs
+++ ProjetoMemorias/Assets/Scripts/SQLite.cs
@@ -2,11 +2,11 @@
 using System.Data;
 using Mono.Data.Sqlite;
 using System.IO;
+using System;
 
 public class SQLite : MonoBehaviour{
     //Esse script é responsável por realizar a conexão entre o banco de dados e o jogo!
-    public static int id_do_save = 1;
-    
+
     // Start is called before the first frame update
     void Start(){
         //CRIANDO A CONEXÃO COM O BANCO DE DADOS
@@ -19,10 +19,9 @@ public class SQLite : MonoBehaviour{
         IDataReader leitor; //Variável responsável por receber o que for retornado (valores...) do banco de dados
 
         dbcmd = dbcon.CreateCommand(); //Criando um comando para o banco de dados 
-        string criar_tabela = "CREATE TABLE IF NOT EXISTS ultimo_save (id INTEGER PRIMARY KEY, tela_salva INTEGER )"; //Escrevendo o comando a ser executado no BD, no caso, será criada uma tabela "ultimo_save" com as colunas "id" e "tela_salva"
+        string criar_tabela = "CREATE TABLE IF NOT EXISTS ultimo_save (id INTEGER PRIMARY KEY AUTOINCREMENT, tela_salva INTEGER )"; //Escrevendo o comando a ser executado no BD, no caso, será criada uma tabela "ultimo_save" com as colunas "id" (com "auto incremento"!) e "tela_salva"
         dbcmd.CommandText = criar_tabela; //Executando o comando da última linha
         leitor = dbcmd.ExecuteReader();
-
     }
 
     // Update is called once per frame
--- ProjetoMemorias/Assets/Scripts/botaoSalvar.cs
+++ ProjetoMemorias/Assets/Scripts/botaoSalvar.cs
@@ -7,63 +7,45 @@ using Mono.Data.Sqlite;
 using System.IO;
 
 public class botaoSalvar : MonoBehaviour{
-    // Start is called before the first frame update
-    void Start(){
-             
-    }
-
-    // Update is called once per frame
+    //Função responsável por salvar o jogo!
     public void salvarJogo(int id_da_fase){
         string connection = "URI=file:"+Application.persistentDataPath+"/BD_Proj_Memorias"; //Adicionando à variável "connection" o caminho para o nosso Banco de Dados "BD_Proj_Memorias"
         IDbConnection dbcon = new SqliteConnection(connection); //Comunicando com o nosso banco de dados cujo caminho está sendo passado como parâmetro pela variável "connection"
         dbcon.Open(); //Abrindo a conexão com o banco de dados
 
-        IDbCommand cmnd = dbcon.CreateCommand();
-        cmnd.CommandText = "INSERT INTO ultimo_save (id, tela_salva) VALUES ("+SQLite.id_do_save+", "+id_da_fase+")";
-        cmnd.ExecuteNonQuery();
+        IDbCommand cmnd = dbcon.CreateCommand(); //Criando variável para receber comando
+        cmnd.CommandText = "INSERT INTO ultimo_save (tela_salva) VALUES ("+id_da_fase+")"; //Aqui, estamos armazenando o ID da fase salva no nosso Banco de DAdos
+        cmnd.ExecuteNonQuery(); //Executando o comando anterior
 
-         //Fechando a conexão
+         //Retornando para o Console os valores salvos na tabela, apenas para teste!
         IDbCommand cmnd_read = dbcon.CreateCommand();
         IDataReader reader;
 
-        string query = "SELECT * FROM ultimo_save";
+        string query = "SELECT * FROM ultimo_save"; //Selecionando todos os itens da nossa tabela no Banco de Dados
         cmnd_read.CommandText = query;
         reader = cmnd_read.ExecuteReader();
 
-        while(reader.Read()){
+        while(reader.Read()){ //Mostrando todos os valores salvos na tabela
             Debug.Log("id: " + reader[0].ToString());
             Debug.Log("última tela salva: " + reader[1].ToString());
         }
-        SQLite.id_do_save = SQLite.id_do_save + 1;
+
+        //Fechando a conexão com o Banco de Dados
         dbcon.Close();
           
     }
 
-    public void limparTabela(){
+    //Função responsável por limpar todos os valores presentes na tabela! Serve para apagar todos os itens (fases) salvos!
+    public void limparTabela(){ 
         string connection = "URI=file:"+Application.persistentDataPath+"/BD_Proj_Memorias"; //Adicionando à variável "connection" o caminho para o nosso Banco de Dados "BD_Proj_Memorias"
         IDbConnection dbcon = new SqliteConnection(connection); //Comunicando com o nosso banco de dados cujo caminho está sendo passado como parâmetro pela variável "connection"
         dbcon.Open(); //Abrindo a conexão com o banco de dados
 
         IDbCommand cmnd = dbcon.CreateCommand();
         cmnd.CommandText = "DELETE FROM ultimo_save";
-        cmnd.ExecuteNonQuery();
+        cmnd.ExecuteNonQuery(); //Executando comando da linha anterior, onde deletaremos todos os itens da nossa tabela "ultimo_save"
 
-        IDbCommand cmnd2 = dbcon.CreateCommand();
-        cmnd2.CommandText = "VACUUM";
-        cmnd2.ExecuteNonQuery();
-
-        IDbCommand cmnd_read = dbcon.CreateCommand();
-        IDataReader reader;
-
-        string query = "SELECT * FROM ultimo_save";
-        cmnd_read.CommandText = query;
-        reader = cmnd_read.ExecuteReader();
-
-        while(reader.Read()){
-            Debug.Log("id: " + reader[0].ToString());
-            Debug.Log("última tela salva: " + reader[1].ToString());
-        }
-
+        //Fechando nossa conexão
         dbcon.Close();
     }
 }
