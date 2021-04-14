365dedcd68f882ff44528aee334d4ce4701c4bac
Oseias Romeiro <78980902+oseias-romeiro@users.noreply.github.com>
2021-04-14 00:13:36 -0300

    script para linkar
    Co-Authored-By: IsaacM-Pereira <65041317+IsaacM-Pereira@users.noreply.github.com>

---
+++ ProjetoMemorias/Assets/Scripts/Notion_Link.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notion_Link : MonoBehaviour
+{
+    public void AbreLink()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
+}
